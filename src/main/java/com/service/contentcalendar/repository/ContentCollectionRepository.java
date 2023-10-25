646002b001f62f89a3454de297f528d40bf170e3
bpriyabhashana <bpriyabhashana@gmail.com>
2023-10-25 13:42:38 +0530

    CRUD operations implementations

--- src/main/java/com/service/contentcalendar/repository/ContentCollectionRepository.java
+++ src/main/java/com/service/contentcalendar/repository/ContentCollectionRepository.java
@@ -15,24 +15,38 @@ import jakarta.annotation.PostConstruct;
 
 @Repository
 public class ContentCollectionRepository {
-    private final List<Content> content = new ArrayList<>();
+    private final List<Content> contentList = new ArrayList<>();
 
     public ContentCollectionRepository() {
 
     }
 
     public List<Content> findAll() {
-        return content;
+        return contentList;
     }
 
     public Optional<Content> findById(Integer id) {
-        return content.stream().filter(c -> c.id().equals(id)).findFirst();
+        return contentList.stream().filter(c -> c.id().equals(id)).findFirst();
+    }
+
+    public void save(Content content) {
+        contentList.removeIf(c -> c.id().equals(content.id()));
+        contentList.add(content);
+    }
+
+    public boolean existsById(Integer id){
+        return contentList.stream().filter(c -> c.id().equals(id)).count() == 1;
+    }
+
+    public void delete(Integer id) {
+        contentList.removeIf(c -> c.id().equals(id));
     }
 
     @PostConstruct
     private void init() {
-        Content c = new Content(
-            1, "My First Blog", 
+        Content content = new Content(
+            1, 
+            "My First Blog", 
             "My first blog post", 
             Status.IDEA, 
             Type.ARTICLE, 
@@ -40,6 +54,6 @@ public class ContentCollectionRepository {
             null, 
             "");
 
-            content.add(c);
+        contentList.add(content);
     }
 }

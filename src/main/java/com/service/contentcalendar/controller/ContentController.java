8a790b747e5c27bf3f3bf34f529570bc1a8e2199
bpriyabhashana <bpriyabhashana@gmail.com>
2023-10-25 12:29:39 +0530

    Dummy data fetching

--- src/main/java/com/service/contentcalendar/controller/ContentController.java
+++ src/main/java/com/service/contentcalendar/controller/ContentController.java
@@ -1,12 +1,27 @@
 package com.service.contentcalendar.controller;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.service.contentcalendar.model.Content;
+import com.service.contentcalendar.repository.ContentCollectionRepository;
 
 @RestController
 @RequestMapping("/api/content")
 public class ContentController {
+
+    private final ContentCollectionRepository repository;
     
-    
+    public ContentController(ContentCollectionRepository repository){
+        this.repository = repository;
+    }
+
+    @GetMapping("")
+    public List<Content> findAll() {
+        return repository.findAll();
+    }
 
 }
--- src/main/java/com/service/contentcalendar/repository/ContentCollectionRepository.java
+++ src/main/java/com/service/contentcalendar/repository/ContentCollectionRepository.java
@@ -1,5 +1,45 @@
 package com.service.contentcalendar.repository;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Repository;
+
+import com.service.contentcalendar.model.Content;
+import com.service.contentcalendar.model.Status;
+import com.service.contentcalendar.model.Type;
+
+import jakarta.annotation.PostConstruct;
+
+@Repository
 public class ContentCollectionRepository {
-    
+    private final List<Content> content = new ArrayList<>();
+
+    public ContentCollectionRepository() {
+
+    }
+
+    public List<Content> findAll() {
+        return content;
+    }
+
+    public Optional<Content> findById(Integer id) {
+        return content.stream().filter(c -> c.id().equals(id)).findFirst();
+    }
+
+    @PostConstruct
+    private void init() {
+        Content c = new Content(
+            1, "My First Blog", 
+            "My first blog post", 
+            Status.IDEA, 
+            Type.ARTICLE, 
+            LocalDateTime.now(), 
+            null, 
+            "");
+
+            content.add(c);
+    }
 }

9889d442901a682b85ea361379d25ee9f96165b9
bpriyabhashana <bpriyabhashana@gmail.com>
2023-11-02 17:56:10 +0530

    Add CORS

--- src/main/java/com/service/contentcalendar/controller/ContentController.java
+++ src/main/java/com/service/contentcalendar/controller/ContentController.java
@@ -3,6 +3,7 @@ package com.service.contentcalendar.controller;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import com.service.contentcalendar.repository.ContentCollectionRepository;
 
 @RestController
 @RequestMapping("/api/content")
+@CrossOrigin
 public class ContentController {
 
     private final ContentCollectionRepository repository;
